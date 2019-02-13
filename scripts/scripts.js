d86e38a6b51c10ed369db611ce9834d3ddf7bfcb
Zoe <zoe_connolly@hotmail.com>
2019-02-12 21:07:58 -0500

    first commit basic set up index, js script, and scss

---
+++ scripts/scripts.js
@@ -0,0 +1,114 @@
+// object
+const trips ={
+    //hot array
+    hot:[
+        {
+            location: "New Orleans",
+            cost: "$",
+            adventures: "yes",
+            thinker: "creative"
+        },
+         {
+            location: "Las Vegas",
+            cost: "$",
+            adventures: "yes",
+            thinker: "logical"
+        },
+         {
+            location: "Key West",
+            cost: "$",
+            adventures: "no",
+            thinker: "creative"
+        },
+         {
+            location: "NASA",
+            cost: "$",
+            adventures: "no",
+            thinker: "logical"
+        },
+         {
+            location: "The Sun",
+            cost: "$$",
+            adventures: "yes",
+            thinker: "creative"
+        },
+         {
+            location: "Galápagos Islands",
+            cost: "$$",
+            adventures: "yes",
+            thinker: "logical"
+        },
+        {
+            location: "Melbourne AU",
+            cost: "$$",
+            adventures: "no",
+            thinker: "creative"
+        },
+        {
+            location: "Silicon Valley",
+            cost: "$$",
+            adventures: "no",
+            thinker: "logical"
+        }
+    ],//end of hot array
+    //cold array
+    cold: [
+        {
+            location: "Yellowknife CA",
+            cost: "$",
+            adventures: "yes",
+            thinker: "creative"
+        },
+        {
+            location: "Jasper Ab CA",
+            cost: "$",
+            adventures: "yes",
+            thinker: "logical"
+        },
+        {
+            location: "winnipeg",
+            cost: "$",
+            adventures: "no",
+            thinker: "creative"
+        },
+        {
+            location: "Sweden",
+            cost: "$",
+            adventures: "no",
+            thinker: "logical"
+        },
+        {
+            location: "Mars",
+            cost: "$$",
+            adventures: "yes",
+            thinker: "creative"
+        },
+        {
+            location: "Iceland",
+            cost: "$$",
+            adventures: "yes",
+            thinker: "logical"
+        },
+        {
+            location: "Montreal CA",
+            cost: "$$",
+            adventures: "no",
+            thinker: "creative"
+        },
+        {
+            location: "Starwars death star",
+            cost: "$$",
+            adventures: "no",
+            thinker: "logical"
+        }
+    ]//end of cold array
+}//end of object
+
+
+// doc ready
+$(function (){
+
+    
+
+})
+
